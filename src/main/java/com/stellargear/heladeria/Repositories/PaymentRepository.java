ac5d73af5340f6e236a4d3b7543e51593472bede
DimeWolf <137341769+Starl1ght5@users.noreply.github.com>
2024-11-23 18:56:37 -0500

    Delete src/main/java/com/stellargear/heladeria/Repositories/PaymentRepository.java

--- src/main/java/com/stellargear/heladeria/Repositories/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stellargear.heladeria.Repositories;
-
-import com.stellargear.heladeria.Models.Entities.Payment;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-
-public interface PaymentRepository extends MongoRepository<Payment, String> {
-}
